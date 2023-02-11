be5c78bf44a408a313dcd4cd8a794f9ed4992eb0
Maria Laura <112260675+mariaLauraDev@users.noreply.github.com>
2023-02-10 21:25:40 -0300

    Update index.js

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ client.on('messageCreate', message => {
     }
 });  
 
-cron.schedule("0 10 12 * * *", () => {
+cron.schedule("0 30 21 * * *", () => {
     let today = new Date();
     let currentMonth = today.getMonth() + 1;
     let currentDay = today.getDate();
